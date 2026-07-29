47fd931ba6600a1c1bdd2158b07e8da73f30a308
Jakub Klejczyk <jklejczyk@gmail.com>
2026-07-29 18:38:05 +0200

    feat: add Machine repository with integration test

---
+++ src/main/java/pl/klejczyk/tpm/machine/domain/MachineRepository.java
@@ -0,0 +1,6 @@
+package pl.klejczyk.tpm.machine.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MachineRepository extends JpaRepository<Machine, String> {
+}
---
+++ src/test/java/pl/klejczyk/tpm/machine/domain/MachineRepositoryIT.java
@@ -0,0 +1,32 @@
+package pl.klejczyk.tpm.machine.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import pl.klejczyk.tpm.machine.TestcontainersConfiguration;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Import(TestcontainersConfiguration.class)
+class MachineRepositoryIT {
+
+    @Autowired
+    private MachineRepository repository;
+
+    @Test
+    void persistsAndRestoresMachineState() {
+        Machine machine = Machine.register("m-1", "Hydraulic press");
+        machine.sendToMaintenance();
+        repository.save(machine);
+
+        Optional<Machine> restored = repository.findById("m-1");
+
+        assertThat(restored).isPresent();
+        assertThat(restored.get().name()).isEqualTo("Hydraulic press");
+        assertThat(restored.get().status()).isEqualTo(MachineStatus.UNDER_MAINTENANCE);
+    }
+}
